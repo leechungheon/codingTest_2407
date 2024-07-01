75abc38f91d718b997c7846f0c582e48e8339cf3
dawn_lch <chung4111@naver.com>
2024-07-01 20:19:35 +0900

    first

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,20 @@
-public class Solution {
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = {};
+        for(int i=0; i<callings.length; i++){
+            for(int j=0; j<players.length; j++){
+                if(callings[i].equals(players[j])){
+                    swap(players,j);
+                }
+            }
+        }
+        answer=players;
+        return answer;
+    }
+    public void swap(String[] players, int j){
+        String temp;
+        temp=players[j];
+        players[j]=players[j-1];
+        players[j-1]=temp;
+    }
 }
