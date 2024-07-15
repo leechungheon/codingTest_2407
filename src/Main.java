07f77a317f8ee6b4b5f962b9ba51541a2474e5ab
dawn_lch <chung4111@naver.com>
2024-07-15 16:09:12 +0900

    first

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 class Main{
     public static void main(String[] args) {
-        String[] s={".#...", "..#..", "...#."};//
+        String today="2022.05.19";
+        String[] terms={"A 6", "B 12", "C 3"};
+        String[] privacies={"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
+        Solution sol=new Solution();
+        sol.solution(today,terms,privacies);
 
     }
 }
